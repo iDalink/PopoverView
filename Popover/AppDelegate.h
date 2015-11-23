1d14ce0fbec882a45a0ef4e178a7956d05c97ef8
Dalink <liuyu@liuyudeMac-mini.local>
2015-11-23 16:16:50 +0800

    New Project

---
+++ Popover/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Popover
+//
+//  Created by liuyu on 14/10/23.
+//  Copyright © 2015年 liuyu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Popover/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Popover
+//
+//  Created by liuyu on 14/10/23.
+//  Copyright © 2015年 liuyu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
